1e6733d3113bc253f8e74ec9107ca8893b1ca36e
Stian <stian.helgerud@outlook.com>
2020-05-01 08:08:05 +0200

    Java Ant build filer

---
+++ Kalkulatorer/src/kalkulatorer/Kalkulatorer.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kalkulatorer;
+
+/**
+ *
+ * @author drizz
+ */
+public class Kalkulatorer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hei Verden");
+    }
+    
+}
